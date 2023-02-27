e6749b3e304597bdbc3d3ade6f091febf71a556f
LDom890 <pedromoralescaba@gmail.com>
2023-02-26 18:35:33 -0600

    Agregando score

---
+++ AR_Test/Assets/Script/LifeScore.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeScore : MonoBehaviour
+{
+    public int score = 0;
+    public Text text;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        text.text = "X " + score.ToString();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+
+            case "Normal":
+
+                score = score + 50;
+                break;
+
+            case "Critic":
+
+                score = score + 100;
+                break;
+
+            case "InstaKill":
+
+                break;
+
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Acid")
+        {
+            score = score + 1;
+        }
+
+
+    }
+}
