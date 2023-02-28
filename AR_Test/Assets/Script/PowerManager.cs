6daec75827c9d52d94a73f944c8df37aadd7991e
Alam Armas <carlosalam33@hotmail.com>
2023-02-27 22:06:03 -0600

    Power

--- AR_Test/Assets/Script/PowerManager.cs
+++ AR_Test/Assets/Script/PowerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -9,16 +10,31 @@ public class PowerManager : MonoBehaviour
     Rigidbody rbGolfy;
     [SerializeField] GameObject cameraT;
     //[SerializeField] Vector3 parabola;
-    [SerializeField] float parabolaForce;
+    [SerializeField] float powerValue, waitingTime;
+    [SerializeField] Slider powerSlider;
+    Vector3 initialPosition;
     private void Start()
     {
-        rbGolfy= GetComponent<Rigidbody>();
+        rbGolfy = GetComponent<Rigidbody>();
         cameraT = GameObject.FindGameObjectWithTag("MainCamera");
+        initialPosition = transform.position;
         //Flying();
     }
     public void Flying()
     {
+        GetForce();
         //parabola = new Vector3(cameraT.transform.position.x,cameraT.transform.position.y * parabolaForce, cameraT.transform.position.z * parabolaForce); 
-        rbGolfy.AddForce(cameraT.transform.forward * parabolaForce, ForceMode.Impulse);
+        rbGolfy.AddForce(cameraT.transform.forward * powerValue, ForceMode.Impulse);
+    }
+    public void GetForce()
+    {
+        powerValue = powerSlider.value;
+        Debug.Log(powerValue);
+        StartCoroutine(RestartPosition(waitingTime));
+    }
+    private IEnumerator RestartPosition(float time)
+    {
+        yield return new WaitForSeconds(time);
+        transform.position = initialPosition;
     }
 }
